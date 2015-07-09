68c02b8aae86e26c836f9341b1216ada314c93b8
Douglas <douglas.ernesto@oi.com.br>
2015-07-09 15:13:47 -0300

    CM_Android_3
    CM_Android_3

---
+++ app/src/androidTest/java/com/example/layout1/lab03/ApplicationTest.java
@@ -0,0 +1,16 @@
+package com.example.layout1.lab03;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+/**
+* @author Douglas da Silva Ernesto
+*/
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
